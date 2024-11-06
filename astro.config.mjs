7369bea0a2d70bf6b96cae485325dd5b080ebeff
GyeongHo Kim <rlarudgh2017@gmail.com>
2024-11-06 22:41:32 +0900

    fix: do not use alias on config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
-import { remarkReadingTime } from '@/utils'
+import { remarkReadingTime } from './src/utils/readTime.ts'
 
 export default defineConfig({
 	// Write here your website url
